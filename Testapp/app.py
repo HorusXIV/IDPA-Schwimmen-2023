ff3261c25ed11da6f9fe187292d2782d55bb4d92
HorusXII <79853368+HorusXII@users.noreply.github.com>
2024-01-06 15:51:40 +0100

    changed time parsing, added constants, improved errorhandling

--- Testapp/app.py
+++ Testapp/app.py
@@ -25,22 +25,13 @@ def parse_custom_time(time_str):
     Raises:
     - ValueError: If the input time string does not match any of the expected formats.
     """
-    # Determine the format of the input time string
-    num_segments = len(str(time_str).split(":"))
+    time_parts_count = len(str(time_str).split(":"))
+    format_map = {1: '00:00:{}', 2: '00:{}', 3: '{}'}
 
-    # Parse the time string based on the detected format
-    if num_segments == 1:
-        # Seconds only format
-        return pd.to_timedelta(f'00:00:{time_str}')
-    elif num_segments == 2:
-        # Minutes:Seconds format
-        return pd.to_timedelta(f'00:{time_str}')
-    elif num_segments == 3:
-        # Hours:Minutes:Seconds format
-        return pd.to_timedelta(time_str)
+    if time_parts_count in format_map:
+        return pd.to_timedelta(format_map[time_parts_count].format(time_str))
     else:
-        # Raise an error for unexpected formats
-        raise ValueError("Invalid time string format. Expected one of: Seconds, Minutes:Seconds, Hours:Minutes:Seconds.")
+        raise ValueError(f'Invalid time format: {time_str}')
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv('SwimDataTop50.csv')
@@ -75,6 +66,18 @@ def fit_rational_function(x, a, b, c):
     # Calculate the output values of the rational function
     return 1 / (a * x + b) + c
 
+# Constants
+DISTANCE_200 = 200
+TRACK_LENGTHS = ["25", "50"]
+TECHNIQUES = ['F', 'R', 'B', 'S', 'L']
+TECHNIQUE_MAP = {
+    'F': 'Freistil',
+    'R': 'Rücken',
+    'B': 'Brust',
+    'S': 'Schmetterling',
+    'L': 'Lagen'
+}
+
 # Define a route for the web application
 @app.route('/requestData')
 def plot_rational_function_for_swimmer():
@@ -96,48 +99,39 @@ def plot_rational_function_for_swimmer():
     # Validate parameters
     if not firstname or not lastname or not track_length or not technique:
         return 'Invalid request. Please provide all parameters.', 400
-    elif technique not in ['F', 'R', 'B', 'S', 'L']:
+    elif technique not in TECHNIQUES:
         return 'Invalid request. Technique must be one of F, R, B, S, L.', 400
-    elif track_length not in ["25", "50"]:
+    elif track_length not in TRACK_LENGTHS:
         return 'Invalid request. Track length must be one of 25, 50.', 400
 
     # Lists to store predicted and measured values
-    mesList = []
-    predList = []
-
-    # Mapping for swimming techniques
-    technique_map = {
-        'F': 'Freistil',
-        'R': 'Rücken',
-        'B': 'Brust',
-        'S': 'Schmetterling',
-        'L': 'Lagen'
-    }
+    measured_values = []
+    predicted_values = []
 
     # Map the technique
-    technique = technique_map[technique]
+    technique = TECHNIQUE_MAP[technique]
 
     # Filter the DataFrame based on the provided parameters
     filtered_df = df[(df['firstname'] == firstname) & (df['surname'] == lastname) & (df['track length'] == int(track_length)) & (df['technique'] == technique)]
 
     # If no data found, return an error message
     if filtered_df.empty:
-        return f'No data found for {firstname} {lastname}.', 400
+        return f'No data found for {firstname} {lastname}', 400
     elif len(filtered_df) < 3:
-        return f'Not enough data found for {firstname} {lastname}.', 400
+        return f'Not enough data found for {firstname} {lastname}. Only {len(filtered_df)} entrys, 3 needed.', 400
     
     if not isAbsolute:
         # Check if 200m distance is present in the filtered data
-        if 200 in filtered_df['distance'].unique():
+        if DISTANCE_200 in filtered_df['distance'].unique():
             # If 200m distance is present, use the speed value for division
-            divisor = filtered_df[filtered_df['distance'] == 200]['speed'].values[0]
+            divisor = filtered_df[filtered_df['distance'] == DISTANCE_200]['speed'].values[0]
         else:
             # If 200m distance is not present, find the closest distance and use its speed value for division
-            closest_distance_index = (filtered_df['distance'] - 200).abs().argsort()[:1]
-            divisor = filtered_df.iloc[closest_distance_index, 7].values[0]
+            closest_distance_index = (filtered_df['distance'] - 200).abs().argsort().sort_values()[:1].index
+            divisor = filtered_df.loc[closest_distance_index, 'speed'].values[0]
 
         # Calculation for relative speed to 200m
-        filtered_df['speed'] = filtered_df['speed'] / divisor
+        filtered_df.loc[:,'speed'] = filtered_df['speed'] / divisor
 
 
     # Fit a rational function to the filtered data
@@ -149,17 +143,17 @@ def plot_rational_function_for_swimmer():
     # Create dictionaries for predicted values
     for X, Y in zip(x_range, y_pred):
         predDict = {"x": X, "y": Y}
-        predList.append(predDict)
+        predicted_values.append(predDict)
 
     # Create dictionaries for measured values
     for distance, speed in zip(filtered_df['distance'], filtered_df['speed']):
         mesDict = {"x": distance, "y": speed}
-        mesList.append(mesDict)
+        measured_values.append(mesDict)
 
     # Create a JSON response containing predicted and measured values
     data = {
-        "pred_values": predList,
-        "mes_values": mesList
+        "pred_values": predicted_values,
+        "mes_values": measured_values
     }
     return jsonify(data), 200
 
@@ -187,4 +181,3 @@ def getAllSwimmers():
 # Run the Flask application
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0', ssl_context=('cert\selfcert.pem', 'cert\selfkey.pem'))
-   
